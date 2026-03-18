0055a84ff42c0785b14b647f350a655f45506c97
harikrushngoti <harikrushn@swamiinfotech.in>
2026-03-18 18:35:04 +0530

    [Add] - inquiey page

--- SBC-Admin/src/app/app.routes.ts
+++ SBC-Admin/src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { Sidebar } from './Component/sidebar/sidebar';
 import { Dashboard } from './Component/dashboard/dashboard';
+import { Enquiries } from './Component/enquiries/enquiries';
 
 export const routes: Routes = [
     {
-        path:"Sidebar",
-        component:Sidebar
+        path: "Sidebar",
+        component: Sidebar
     },
     {
-        path:"Dashboard",
-        component:Dashboard
+        path: "Dashboard",
+        component: Dashboard
+    },
+    {
+        path: "Customer",
+        component: Enquiries
     }
 ];
